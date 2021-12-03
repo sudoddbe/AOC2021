0928b7dcb121bd9811d169bdb62220d1b5c5e781
David Belfrage <david.belfrage@hotmail.com>
2021-12-03 23:14:47 +0100

    Day3: Part 2 electric boogaloo

--- day3/main.py
+++ day3/main.py
@@ -1,10 +1,30 @@
 import numpy as np
 
-data = np.genfromtxt('data', delimiter=1, dtype=np.float64) 
-omega = np.mean(data, axis = 0) > 0.5
+data = np.genfromtxt('data', delimiter=1, dtype=np.float64)
+omega = np.mean(data, axis = 0) >= 0.5
 epsilon = ~omega
-binary_base = np.arange(omega.shape[0])
-omega_decimal = np.dot(2**np.arange(omega.shape[0])[::-1], omega)
-epsilon_decimal = np.dot(2**np.arange(epsilon.shape[0])[::-1], epsilon)
+binary_base = 2**np.arange(omega.shape[0])[::-1]
+omega_decimal = np.dot(binary_base, omega)
+epsilon_decimal = np.dot(binary_base, epsilon)
 power = epsilon_decimal * omega_decimal
-print(power)
+
+index = np.arange(data.shape[0])
+col = 0
+while(index.shape[0] != 1):
+    bv = np.mean(data[index, col]) >= 0.5
+    index = index[data[index, col] == bv]
+    col += 1
+oxygen = data[index,:][0]
+
+index = np.arange(data.shape[0])
+col = 0
+while(index.shape[0] != 1):
+    bv = np.mean(data[index, col]) < 0.5
+    index = index[data[index, col] == bv]
+    col += 1
+scrubber = data[index,:][0]
+
+oxygen_decimal = np.dot(binary_base, oxygen)
+scrubber_decimal = np.dot(binary_base, scrubber)
+life_support = scrubber_decimal * oxygen_decimal
+print(life_support)
